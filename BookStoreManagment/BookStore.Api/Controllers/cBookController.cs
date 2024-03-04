dd4b70bcf385a826b034184e0f0464da61de277a
Abu_Programmiy <abdukholiq23@gmail.com>
2024-03-04 07:31:40 +0500

    yuklandi

---
+++ BookStoreManagment/BookStore.Api/Controllers/cBookController.cs
@@ -0,0 +1,59 @@
+﻿using BookStore.Api.Attributes;
+using BookStore.Application.Abstractions.IServices;
+using BookStore.Domain.Entities.DTOs;       // BookDTO ishlashi uchun
+using BookStore.Domain.Entities.Enums;
+using BookStore.Domain.Entities.Models;     // Book ishlashi uchun
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;             // ControllerBase ishlashi uchun
+
+namespace BookStore.Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    [Authorize]
+    public class cBookController : ControllerBase
+    {
+        private readonly IBookService _bookService;
+
+        public cBookController(IBookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+        [HttpPost]
+        [IdentityFilter(Permissions.Create)]
+        public string Create(BookDTO bookDTO)
+        {
+            return _bookService.Create(bookDTO);
+        }
+
+        [HttpGet]
+        [IdentityFilter(Permissions.Get)]
+        public IEnumerable<Book> GetAll()
+        {
+            return _bookService.GetAll();
+        }
+
+        [HttpGet]
+        [IdentityFilter(Permissions.Get)]
+        public Book GetByName(string Name)
+        {
+            return _bookService.GetByName(Name);
+        }
+
+        [HttpPut]
+        [IdentityFilter(Permissions.Update)]
+        public string Update(int id,BookDTO bookDTO)
+        {
+            return _bookService.Update(id, bookDTO);
+        }
+
+        [HttpDelete]
+        [IdentityFilter(Permissions.Delete)]
+        public string Delete(int id)
+        {
+            return _bookService.Delete(id);
+        }
+    }
+}
---
+++ BookStoreManagment/BookStore.Infrastructure/BaseRepositories/BaseRepository.cs
@@ -0,0 +1,56 @@
+﻿using BookStore.Application.Abstractions;       // DbSet ishlashi uchun
+using BookStore.Infrastructure.Persistance;     // BookStoreDbContext ishlashi uchun
+using Microsoft.EntityFrameworkCore;            // DbSet ishlashi uchun
+using System.Linq.Expressions;                  // Expression ishlashi uchun
+
+namespace BookStore.Infrastructure.BaseRepositories
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : class
+    {
+        private readonly BookStoreDbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public BaseRepository(BookStoreDbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+        public string Create(T model)
+        {
+            _dbSet.AddAsync(model).GetAwaiter().GetResult();
+            _context.SaveChangesAsync().GetAwaiter().GetResult();
+
+            return "Create qilindi!";
+        }
+
+        public string Delete(Expression<Func<T, bool>> expression)
+        {
+            T model = _dbSet.FirstOrDefaultAsync(expression).GetAwaiter().GetResult()!;
+            if (model == null)
+            {
+                return "Ma'lumot topilmadi.\nShuning uchun hechnima ham ochirilmadi)";
+            }
+            _dbSet.Remove(model);
+            _context.SaveChangesAsync().GetAwaiter().GetResult();
+            return "Malumot ochirildi!";
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _dbSet.ToListAsync().GetAwaiter().GetResult();
+        }
+
+        public T GetByAny(Expression<Func<T, bool>> expression)
+        {
+            return _dbSet.FirstOrDefault(expression)!;
+        }
+
+        public string Update(T model)
+        {
+            _dbSet.Update(model);
+            _context.SaveChangesAsync().GetAwaiter().GetResult();
+            return "Update muvaffaqiyat topdi!";
+        }
+    }
+}
